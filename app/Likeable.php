267aa2b149206201bb244a4a8432ddd872f2c899
Harsh <agarwalhasrh414@gmail.com>
2021-05-03 21:25:02 +0530

    newway

--- app/Likeable.php
+++ app/Likeable.php
@@ -11,8 +11,13 @@ trait Likeable
 
     public function ifLikedBy(User $user,Tweet $tweet)
     {
-        return !!$user->likes()->where('likes.tweet_id', $tweet->id)
-            ->where('likes.isLiked', true)->count();
+        $likeOrNot = DB::table('likes')->where('tweet_id', $tweet->id)
+            ->where('user_id',$user->id)
+            ->where('isLiked', true)->get();
+        if(count($likeOrNot)) return 1;
+        else return 0;
+//        return !!$user->likes()->where('likes.tweet_id', $tweet->id)
+//            ->where('likes.isLiked', true)->count();
     }
 
     public function like($user = null,  $liked = true)   // for liking a tweet
