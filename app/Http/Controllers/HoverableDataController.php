b72182b660437d40ddd5103993aae111fc67486c
Harsh <agarwalhasrh414@gmail.com>
2021-05-30 23:37:11 +0530

    modalChanges

--- app/Http/Controllers/HoverableDataController.php
+++ app/Http/Controllers/HoverableDataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class HoverableDataController extends Controller
@@ -14,6 +15,7 @@ class HoverableDataController extends Controller
         $output="";
         $u_id= $request->get('id');
         $user= User::findOrFail($u_id);
+        $auth_user= Auth::user();
         if(auth()->user()->isFollowing($user)){
             $s1="Unfollow";
             $s2="Following";
@@ -30,23 +32,26 @@ class HoverableDataController extends Controller
             <div>
                 <img src="'.$user->url.'" style="height: 50px;width: 50px;" alt="" class="rounded-full mr-2">
             </div>';
-            if(auth()->user()->isFollowing($user)) {
-                $output .= '
-            <form action="'. 'tweets/' . $user->id . '/unfollow' . '" method="POST">';
-            }else {
-                $output .= '
+            if($user!=$auth_user) {
+                if (auth()->user()->isFollowing($user)) {
+                    $output .= '
+            <form action="' . 'tweets/' . $user->id . '/unfollow' . '" method="POST">';
+                } else {
+                    $output .= '
             <form action="' . 'tweets/' . $user->id . '/follow' . '" method="POST">';
-            }
-            $output.='
-            '.@csrf_field().'
-            '.method_field("POST").'
-            <button type="submit" title="'.$s1.'"
-                        value="'.$s2.'" id="follow"
+                }
+                $output .= '
+            ' . @csrf_field() . '
+            ' . method_field("POST") . '
+            <button type="submit" title="' . $s1 . '"
+                        value="' . $s2 . '" id="follow"
                         class="hover:bg-black rounded-lg rounded-lg p-2 font-bold text-blue-400"
                         style="border:2px solid lightskyblue;outline: none; border-radius: 20px !important;">
-                    '.$s2.'
+                    ' . $s2 . '
                 </button>
-             </form>
+             </form>';
+            }
+            $output.='
         </div>
         <div class="text-sm font-bold mt-2 pl-1 pr-1">
             <a href="profile/'.$user->id.'" style="text-decoration:none;color:black;">
