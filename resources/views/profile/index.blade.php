f973ccfe4e284176a8a4987e0f339d76f3e9cb37
Harsh <agarwalhasrh414@gmail.com>
2021-05-31 00:19:27 +0530

    changes

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -49,7 +49,7 @@ border-right-color: white; border-bottom-color: white " data-toggle="modal" data
     </form>
     @endif
 </div>
-
+<div class="mb-2">{{$profile->bio}}</div>
 
 <div class="flex gap-2">
     <img src="{{asset('images/calendar.png')}}" style="height: 20px; width: 20px" alt="">
